5a2d9caab9650bce4d57f7d781a80b2f69bfd757
gregmurray <gd-murray@hotmail.com>
2020-03-03 15:37:21 -0500

    default react app what

--- src/containers/Admin/AdminHome.js
+++ src/containers/Admin/AdminHome.js
@@ -1,26 +1,31 @@
+/* eslint-disable no-loop-func */
 import React, { Component } from "react";
 import { push } from "connected-react-router";
 import { connect } from "react-redux";
-import {mapsRef} from "../../firebase";
+import { mapsRef } from "../../firebase";
 import mapData from "../../maps.json";
 
 class Admin extends Component {
-    loadMapData(){
-        for(var map of mapData){
-            mapsRef.add(map);
-        }
-        alert(`Inserted: ${mapData.length} items`)
-    }
+    
+    
     render() {
         return (
             <div>
                 <h1>Admin Page</h1>
                 <button onClick={() => this.props.goToRoute("/admin/maps")}>Go to Maps</button>
-                <button onClick={() => this.loadMapData()}>Insert Map Data</button>
             </div>
         )
     }
 }
+/*
+loadMapData() {
+        for (var map of mapData) {
+            mapsRef.add(map)
+        }
+        alert(`Inserted maps`)
+    }
+<button onClick={() => this.loadMapData()}>Insert Map Data</button>
+*/
 
 
 const mapDispatchToProps = (dispatch) => ({
